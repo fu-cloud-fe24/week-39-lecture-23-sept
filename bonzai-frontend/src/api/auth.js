488ae80acb276b9776120c926b14e36285ef7e59
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-09-23 09:58:04 +0200

    added finished code

--- bonzai-frontend/src/api/auth.js
+++ bonzai-frontend/src/api/auth.js
@@ -1,9 +1,29 @@
 import axios from 'axios';
 
 export const loginApi = async (data) => {
-
+    const response = await axios.post('https://zrj3mqpb1e.execute-api.eu-north-1.amazonaws.com/api/auth/login',
+        data
+    )
+    .then(response => { return response; })
+    .catch(error => { return error; });
+    
+    if(response.status === 200) {
+        return response;
+    } else {
+        return response.data.message;
+    }
 }
 
 export const registerApi = async (data) => {
-
+    const response = await axios.post('https://zrj3mqpb1e.execute-api.eu-north-1.amazonaws.com/api/auth/register',
+        data
+    )
+    .then(response => { return response; })
+    .catch(error => { return error; });
+    
+    if(response.status === 201) {
+        return response;
+    } else {
+        return response.data.message;
+    }
 }
--- bonzai-frontend/src/api/bookings.js
+++ bonzai-frontend/src/api/bookings.js
@@ -1,13 +1,62 @@
 import axios from 'axios';
 
 export const postBooking = async (data, token) => {
+    const response = await axios.post('https://zrj3mqpb1e.execute-api.eu-north-1.amazonaws.com/api/bookings',
+        data,
+        {
+            headers : {
+                Authorization : token,
+                'Content-Type' : 'application/json'
+            }
+        }
+    )
+    .then(response => { return response; })
+    .catch(error => { return error; });
 
+    if(response.status === 200) {
+        return response;
+    } else {
+        return response.response.data.message;
+    }
 }
 
 export const getBookings = async (data, token) => {
+    const response = await axios.get('https://zrj3mqpb1e.execute-api.eu-north-1.amazonaws.com/api/bookings',
+        {
+            headers : {
+                Authorization : token,
+                'Content-Type' : 'application/json'
+            }
+        }
+    )
+    .then(response => { return response; })
+    .catch(error => { return error; });
 
+    if(response.status === 200) {
+        return response;
+    } else {
+        return response.data.message;
+    }
 }
 
 export const cancelBooking = async (id, token) => {
+    const response = await axios.patch('https://zrj3mqpb1e.execute-api.eu-north-1.amazonaws.com/api/bookings/' + id,
+        {
+            status : 'CANCELLED'
+        },
+        {
+            headers : {
+                Authorization : token,
+                'Content-Type' : 'application/json'
+            }
+        }
+    )
+    .then(response => { return response; })
+    .catch(error => { return error; });
 
+    if(response.status === 201) {
+        return response;
+    } else {
+        return response.data.message;
+    }
 }
